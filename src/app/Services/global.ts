82c6be3ead70f9e1b77c5847a52813f2ae117d5b
Jhon Torres <fre920@hotmail.com>
2020-10-29 17:57:54 -0500

    fix code
    Fix Code

--- src/app/Services/global.ts
+++ src/app/Services/global.ts
@@ -1,3 +1,5 @@
 export var GLOBAL ={
-    url: 'http://35.175.112.124:8080/api/v1/question/'
+   //url: 'http://35.175.112.124:8080/api/v1/question/'
+
+    url:  'http://127.0.0.1:5000/api/v1/question/'
 }
